cc17f9fc8584246c19cc9395503d4b8bcbd52b87
Dares34 <GorodiskiyDen@gmail.com>
2024-12-18 23:29:51 +0300

    home page update

--- dencha-music/src/components/Home.jsx
+++ dencha-music/src/components/Home.jsx
@@ -48,13 +48,11 @@ const Home = () => {
           <div className="site-body-middle">
             <h2 className="overlay-text" id="wave-text">► Моя волна</h2>
             <div className="tenor-gif-embed" data-postid="25192894" data-share-method="host" data-aspect-ratio="1" data-width="100%">
-              <a href="https://tenor.com/view/amalie-steiness-borregaard-loading-gif-loading-gif-25192894">
-                Amalie Steiness GIF
-              </a> from{" "}
-              <a href="https://tenor.com/search/amalie-gifs">Amalie GIFs</a>
+              <a href="https://tenor.com/view/amalie-steiness-borregaard-loading-gif-loading-gif-25192894">Amalie Steiness GIF</a>
+              from <a href="https://tenor.com/search/amalie-gifs">Amalie GIFs</a>
             </div>
           </div>
-          <Slider title="Вы недавно слушали" />
+          <Slider/>
         </div>
         <div className="right-body">
           <Playlist />
--- dencha-music/src/components/Slider.jsx
+++ dencha-music/src/components/Slider.jsx
@@ -2,23 +2,64 @@ import React from "react";
 
 const Slider = ({ title }) => {
   return (
-    <div className="slider">
-      <h1>{title}</h1>
-      <div className="slider-wrapper">
-        <div className="slide">
-          <img src="./assets/images/premiere.png" alt="Image 1" />
-          <p>Премьера</p>
-          <p style={{ color: "#585858" }}>Открывает вам главные новинки</p>
+    <div className="site-body-down">
+      {/* Первый слайдер */}
+      <div className="slider">
+        {/* <h1>{title || "Список плейлистов"}</h1> */}
+        <div className="slider-wrapper">
+          <div className="slide">
+            <img src={`${process.env.PUBLIC_URL}/assets/images/premiere.png`} alt="Image 1" />
+            <p>Премьера</p>
+            <p style={{ color: "#585858" }}>Открывает вам главные новинки</p>
+          </div>
+          <div className="slide">
+            <img src={`${process.env.PUBLIC_URL}/assets/images/dejavu.png`} alt="Image 2" />
+            <p>Дежавю</p>
+            <p style={{ color: "#585858" }}>Знакомит с тем, что вы ещё не слушали</p>
+          </div>
+          <div className="slide">
+            <img src={`${process.env.PUBLIC_URL}/assets/images/stash.png`} alt="Image 3" />
+            <p>Тайник</p>
+            <p style={{ color: "#585858" }}>Достаёт забытое из вашей коллекции</p>
+          </div>
+          <div className="slide">
+            <img src={`${process.env.PUBLIC_URL}/assets/images/playlistofday.png`} alt="Image 4" />
+            <p>Плейлист дня</p>
+            <p style={{ color: "#585858" }}>Звучит по высшему каждый день</p>
+          </div>
         </div>
-        <div className="slide">
-          <img src="./assets/images/dejavu.png" alt="Image 2" />
-          <p>Дежавю</p>
-          <p style={{ color: "#585858" }}>Знакомит с тем, что вы ещё не слушали</p>
-        </div>
-        {/* Add more slides as needed */}
+        <a className="prev">&#10094;</a>
+        <a className="next">&#10095;</a>
       </div>
-      <a className="prev">&#10094;</a>
-      <a className="next">&#10095;</a>
+
+      {/* Второй слайдер */}
+      <div className="slider">
+        <h1>Вы недавно слушали</h1>
+        <div className="slider-wrapper">
+          <div className="slide">
+            <img src={`${process.env.PUBLIC_URL}/assets/images/premiere.png`} alt="Image 1" />
+            <p>Премьера</p>
+            <p style={{ color: "#585858" }}>Открывает вам главные новинки</p>
+          </div>
+          <div className="slide">
+            <img src={`${process.env.PUBLIC_URL}/assets/images/dejavu.png`} alt="Image 2" />
+            <p>Дежавю</p>
+            <p style={{ color: "#585858" }}>Знакомит с тем, что вы ещё не слушали</p>
+          </div>
+          <div className="slide">
+            <img src={`${process.env.PUBLIC_URL}/assets/images/stash.png`} alt="Image 3" />
+            <p>Тайник</p>
+            <p style={{ color: "#585858" }}>Достаёт забытое из вашей коллекции</p>
+          </div>
+          <div className="slide">
+            <img src={`${process.env.PUBLIC_URL}/assets/images/playlistofday.png`} alt="Image 4" />
+            <p>Плейлист дня</p>
+            <p style={{ color: "#585858" }}>Звучит по высшему каждый день</p>
+          </div>
+        </div>
+        <a className="prev-list">&#10094;</a>
+        <a className="next-list">&#10095;</a>
+      </div>
     </div>
   );
 };
